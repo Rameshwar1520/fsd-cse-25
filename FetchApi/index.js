b389da97e0d48ff291a25d174e28a5433f5a38ee
Rameshwar1520 <rameshwart311@gmail.com>
2026-09-01 10:02:33 +0530

    Add FetchApi project

---
+++ FetchApi/index.js
@@ -0,0 +1,34 @@
+// console.log(hiii)
+const root = document.getElementById("container");
+const button = document.getElementById("btn");
+const display = document.getElementById("display");
+const loader = document.getElementById("loader");
+
+async function fetchData() {
+    try {
+        loader.textContent = "Loading...";
+        const serverData = await fetch("https://fakestoreapi.com/products/");
+        const jsonData = await serverData.json();
+
+        let table = `<table border="2px">
+            ${jsonData.map((item) => `
+                <tr>
+                    <td>${item.id}</td>
+                    <td>${item.title}</td>
+                    <td>${item.price}</td>
+                    <td><img src="${item.image}" alt="${item.title}" width="100"></td>
+                    <td>${item.description}</td>
+                </tr>
+            `)}
+
+        </table>`;
+
+        display.innerHTML = table;
+    } catch (error) {
+        console.log("error is" + error);
+    } finally {
+        loader.textContent = "";
+    }
+}
+
+button.addEventListener("click", fetchData);
